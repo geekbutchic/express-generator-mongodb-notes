2736e5913ebeb1e831e00904ba40032f6b01cd38
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2021-05-28 23:29:13 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+// VIEW ENGINE SETUP
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+// ACCEPT IN FORM DATA
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// CATCH 404 AND FORWARD TO ERROR HANDLER
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// ERROR HANDLER
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // RENDER THE ERROR PAGE
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ readme.md
@@ -0,0 +1,21 @@
+# `SETTING UP EXPRESS`
+
+### `TERMINAL/PAGE SETUP`
+
+`STEP 1.` 
+
+* IN TERMINAL ADD -->
+* `express (project name) --view=ejs`
+
+```javascript
+//Example
+express express-generator-demo --view=ejs
+```
+`STEP 2.` 
+* `npm i`
+
+`STEP 3.`
+* IN TERMINAL RUN `nodemon`
+
+`STEP 4`
+* 
